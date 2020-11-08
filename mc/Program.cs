de1b6e887738cbf4ab7da1206ff3af7f1fe1eaf1
Samyak Maharjan <samyakjyapu33127@gmail.com>
2020-11-08 13:59:25 +0545

    setup project

---
+++ mc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace mc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+                Console.Write("> ");
+                string line = Console.ReadLine();
+                if(string.IsNullOrWhiteSpace(line))
+                    return;
+                
+                if(line == "1 * 3 + 1")
+                    Console.WriteLine("7");
+                else
+                    Console.WriteLine("Error: lala");
+            }
+            
+        }
+    }
+
+    class Lexer
+    {
+        public Lexer(string text)
+        {
+            
+        }
+
+    }
+}
